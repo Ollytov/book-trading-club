4089ea33acfdba79b375c7528d6341bdc63dd426
Ollytov <daniel.maloof@gmail.com>
2015-11-07 23:53:35 -0500

    main page html updated

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/booktradingclub-dev'
+    uri: 'mongodb://meanuser:meanpass@ds037244.mongolab.com:37244/heroku_rmlfs694'
   },
 
   seedDB: true
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,6 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/booktradingclub'
+            'mongodb://meanuser:meanpass@ds037244.mongolab.com:37244/heroku_rmlfs694'
   }
 };
